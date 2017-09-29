58e1d7b38fd180c79ea6d129d325aa0e213b9826
McShi <rashida.mcwilliams@gmail.com>
2017-09-29 14:23:51 -0400

    Completed Project

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -10,6 +10,134 @@ namespace Project_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine().Trim();
+            firstName = firstName.ToLower();
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine().Trim();
+            lastName = lastName.ToLower();
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What month were you born? Two digits, please.");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Gimme your fav ROYGBIV.If you're confused, enter 'Help' for assistance.");
+            string favColor = Console.ReadLine().Trim();
+            favColor = favColor.ToLower();
+
+            if (favColor == "help")
+            {
+                Console.WriteLine("ROYGBIV means Red, Orange, Yellow, Green, Blue, Indigo and Violet");
+                Console.WriteLine("Please re-enter one of the listed colors.");
+                favColor = Console.ReadLine().Trim().ToLower();
+
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("How many siblings do you have?");
+            int userSiblings = int.Parse(Console.ReadLine());
+
+            string retirementYears;
+
+            if (userAge / 2 == 0)
+            {
+                retirementYears = "7 years";
+            }
+            else
+            {
+                retirementYears = "8 years";
+            }
+
+            string retirementFunds;
+
+            if (birthMonth <= 4)
+            {
+                retirementFunds = "$25";
+            }
+            else if (birthMonth > 4 && birthMonth < 9)
+            {
+                retirementFunds = "$5 million";
+            }
+            else if (birthMonth > 8 && birthMonth < 13)
+            {
+                retirementFunds = "$4,738";
+            }
+            else
+            {
+                retirementFunds = "$0.00";
+            }
+
+
+            string modeTransport;
+
+            switch(favColor)
+            {
+                case "red":
+                    modeTransport = "unicycle";
+                    break;
+                case "orange":
+                    modeTransport = "zeppelin";
+                    break;
+                case "yellow":
+                    modeTransport = "horse-drawn carriage";
+                    break;
+                case "green":
+                    modeTransport = "steam engine";
+                    break;
+                case "blue":
+                    modeTransport = "camel";
+                    break;
+                case "indigo":
+                    modeTransport = "skateboard";
+                    break;
+                case "violet":
+                    modeTransport = "dogsled";
+                    break;
+                default:
+                    modeTransport = "canoe";
+                    break;
+            }
+
+            string vacationHome;
+            if (userSiblings == 0)
+            {
+                vacationHome = "Bali";
+            }
+            else if (userSiblings == 1)
+            {
+                vacationHome = "Taos";
+            }
+            else if (userSiblings == 2)
+            {
+                vacationHome = "New Zealand";
+            }
+            else if (userSiblings == 3)
+            {
+                vacationHome = "Buenos Aires";
+            }    
+            else if (userSiblings > 3)
+            {
+                vacationHome = "Tanzania";
+            }
+            else
+            {
+                vacationHome = "Scranton, PA";
+            }
+
+            Console.WriteLine(firstName + " " + lastName + " will retire in " + retirementYears + " with " + retirementFunds + " in the bank, a vacation home in " + vacationHome + " and a " + modeTransport + ".");
+             
+                    
+            
+
+
+
+
         }
     }
 }
